ae3a14691f2c6f5e635c5e5e601f378d92dfe3c0
Keeley Hoek <escortkeel@gmail.com>
2013-02-21 14:21:13 +1100

    Added compiler build script (stolen from boomstick)

---
+++ compiler/run.sh
@@ -0,0 +1,95 @@
+OSNAME=K-OS
+
+BINUTILS_VER=2.20
+GCC_VER=4.5.0
+GMP_VER=5.0.1
+MPFR_VER=2.4.2
+MPC_VER=0.8.1
+
+export TARGET=i586-elf
+export PREFIX=`pwd`/local
+
+mkdir -p build
+mkdir -p local
+cd build
+
+WFLAGS=-c
+
+export PATH=$PREFIX/bin:$PATH
+
+echo "FETCH BINUTILS"
+wget $WFLAGS http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VER}.tar.gz
+tar -xf binutils-${BINUTILS_VER}.tar.gz
+
+echo "FETCH GCC"
+wget $WFLAGS http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-core-${GCC_VER}.tar.gz
+tar -xf gcc-core-${GCC_VER}.tar.gz
+wget $WFLAGS http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-g++-${GCC_VER}.tar.gz
+tar -xf gcc-g++-${GCC_VER}.tar.gz
+
+echo "FETCH GMP"
+wget $WFLAGS http://ftp.gnu.org/gnu/gmp/gmp-${GMP_VER}.tar.gz
+tar -xf gmp-${GMP_VER}.tar.gz
+
+echo "FETCH MPFR"
+wget $WFLAGS http://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VER}.tar.gz
+tar -xf mpfr-${MPFR_VER}.tar.gz
+
+echo "FETCH MPC"
+wget $WFLAGS http://www.multiprecision.org/mpc/download/mpc-${MPC_VER}.tar.gz
+tar -xf mpc-${MPC_VER}.tar.gz
+
+echo "MAKE OBJECT DIRECTORIES"
+mkdir -p binutils-obj
+mkdir -p gcc-obj
+mkdir -p newlib-obj
+mkdir -p gmp-obj
+mkdir -p mpfr-obj
+mkdir -p mpc-obj
+
+echo "COMPILE BINUTILS"
+cd binutils-obj
+../binutils-${BINUTILS_VER}/configure --target=$TARGET --prefix=$PREFIX || exit
+make || exit
+make install || exit
+cd ..
+
+echo "COMPILE GMP"
+cd gmp-obj
+../gmp-${GMP_VER}/configure --prefix=$PREFIX --disable-shared || exit
+make || exit
+make check || exit
+make install || exit
+cd ..
+
+echo "COMPILE MPFR"
+cd mpfr-obj
+../mpfr-${MPFR_VER}/configure --prefix=$PREFIX --with-gmp=$PREFIX --disable-shared
+make || exit
+make check || exit
+make install || exit
+cd ..
+
+echo "COMPILE MPC"
+cd mpc-obj
+../mpc-${MPC_VER}/configure --target=$TARGET --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --disable-shared || exit
+make || exit
+make check || exit
+make install || exit
+cd ..
+
+
+echo "AUTOCONF GCC"
+cd gcc-${GCC_VER}/libstdc++-v3
+#autoconf || exit
+cd ../..
+
+echo "COMPILE GCC"
+cd gcc-obj
+../gcc-${GCC_VER}/configure --target=$TARGET --prefix=$PREFIX --enable-languages=c,c++ --without-headers --disable-nls --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-mpc=$PREFIX || exit
+
+make all-gcc || exit
+make install-gcc || exit
+cd ..
+
+echo "DONE"
