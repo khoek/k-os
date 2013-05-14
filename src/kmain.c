a4e945b24a372c2e79909997f7ff7415416fff6b
Keeley Hoek <escortkeel@gmail.com>
2013-05-14 15:22:14 +1000

    Added version numbers

--- src/kmain.c
+++ src/kmain.c
@@ -1,21 +1,22 @@
 #include <stdint.h>
 #include "multiboot.h"
-#include "elf.h"
-#include "panic.h"
+#include "version.h"
 #include "console.h"
+#include "panic.h"
+#include "elf.h"
 #include "gdt.h"
 #include "idt.h"
+#include "keyboard.h"
 #include "pit.h"
 #include "mm.h"
 #include "pci.h"
-#include "keyboard.h"
 
 #define PIT_FREQ 100
 
 void kmain(uint32_t magic, multiboot_info_t *mbd) {
     console_clear();
 
-    kprintf("Starting K-OS...\n\n");
+    kprintf("Starting K-OS (v%u.%u.%u)...\n\n", MAJOR, MINOR, PATCH);
     if (magic != MULTIBOOT_BOOTLOADER_MAGIC)    panic("multiboot loader did not pass correct magic number");
     if (!(mbd->flags & MULTIBOOT_INFO_MEMORY))  panic("multiboot loader did not pass memory information");
     if (!(mbd->flags & MULTIBOOT_INFO_MEM_MAP)) panic("multiboot loader did not pass memory map");
@@ -37,16 +38,8 @@ void kmain(uint32_t magic, multiboot_info_t *mbd) {
     kprintf("\nProbing PCI...\n");
     pci_init();
 
-    kprintf("\n");
-    for(uint8_t seconds = 5; seconds > 0; seconds--) {
-        kprintf("Exploding... %u\r", seconds);
-        sleep(100);
-    }
-    kprintf("Exploding... 0\r");
-    sleep(10);
-    kprintf("Exploding... BOOM!");
-
-    __asm__ volatile("int $0x80");
-
+    kprintf("\nDone.");
+    
+    die();
     panic("kmain returned!");
 }
