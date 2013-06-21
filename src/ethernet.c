58005c4a29c2b04b6cc62d52bfa2db74c9f4bcac
Keeley Hoek <escortkeel@gmail.com>
2013-06-21 21:50:20 +1000

    Added UDP parsing

--- src/ethernet.c
+++ src/ethernet.c
@@ -34,19 +34,12 @@ typedef struct ip_header {
     uint8_t dst[4];
 } PACKED ip_header_t;
 
-#define TCP_DATA_OFF(x) ((x & 0xF000) >> 4)
-#define TCP_FLAGS(x)    ((x & 0x0FFF) >> 4)
-
-typedef struct tcp_header {
+typedef struct udp_header {
     uint16_t src_port;
     uint16_t dst_port;
-    uint32_t seq_num;
-    uint32_t ack_num;
-    uint16_t data_off_flags;
-    uint16_t window_size;
+    uint16_t length;
     uint16_t checksum;
-    uint16_t urgent;
-} PACKED tcp_header_t;
+} PACKED udp_header_t;
 
 void ethernet_handle(uint8_t *packet, uint16_t length) {
     ethernet_header_t *header = (ethernet_header_t *) packet;
@@ -79,7 +72,15 @@ void ethernet_handle(uint8_t *packet, uint16_t length) {
                         break;
                     }
                     case IP_TYPE_UDP: {
-                        //logf("ethernet - udp packet detected");
+                        udp_header_t *udp = (udp_header_t *) packet;
+                        packet += sizeof(udp_header_t);
+                        length -= sizeof(udp_header_t);
+                        
+                        logf("ethernet - udp packet detected");
+                        logf("ethernet - src %u.%u.%u.%u port %u dst %u.%u.%u.%u port %u",
+                            ip->src[0], ip->src[1], ip->src[2], ip->src[3], udp->src_port,
+                            ip->dst[0], ip->dst[1], ip->dst[2], ip->dst[3], udp->dst_port
+                        );
                         break;
                     }
                     default: {
