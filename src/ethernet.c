a414049756833e9886ab8cfce71fc6d042dc6e4b
Keeley Hoek <escortkeel@gmail.com>
2013-06-21 21:39:56 +1000

    Added TCP parsing

--- src/ethernet.c
+++ src/ethernet.c
@@ -2,12 +2,12 @@
 #include "ethernet.h"
 #include "log.h"
 
-#define TYPE_IP     0x0008
-#define TYPE_ARP    0x0608
+#define ETHERNET_TYPE_IP    0x0008
+#define ETHERNET_TYPE_ARP   0x0608
 
 typedef struct ethernet_header {
-    uint8_t dst[6];
-    uint8_t src[6];
+    uint8_t  dst[6];
+    uint8_t  src[6];
     uint16_t type;
 } PACKED ethernet_header_t;
 
@@ -18,44 +18,85 @@ typedef struct ethernet_header {
 #define IP_FLAGS(x)     ((x & 0x00E0) >> 5)
 #define IP_FRAG_OFF(x)  ((x & 0xFF1F))
 
+#define IP_TYPE_TCP 0x06
+#define IP_TYPE_UDP 0x11
+
 typedef struct ip_header {
-    uint8_t version_ihl;
-    uint8_t dscp_ecn;
+    uint8_t  version_ihl;
+    uint8_t  dscp_ecn;
     uint16_t total_length;
     uint16_t ident;
     uint16_t flags_frag_off;
-    uint8_t ttl;
-    uint8_t protocol;
+    uint8_t  ttl;
+    uint8_t  protocol;
     uint16_t checksum;
-    uint32_t src;
-    uint32_t dst;
+    uint8_t src[4];
+    uint8_t dst[4];
 } PACKED ip_header_t;
+
+#define TCP_DATA_OFF(x) ((x & 0xF000) >> 4)
+#define TCP_FLAGS(x)    ((x & 0x0FFF) >> 4)
+
+typedef struct tcp_header {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint16_t data_off_flags;
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urgent;
+} PACKED tcp_header_t;
 
 void ethernet_handle(uint8_t *packet, uint16_t length) {
     ethernet_header_t *header = (ethernet_header_t *) packet;
     packet += sizeof(ethernet_header_t);
-
-    logf("ethernet - packet type:0x%02X recieved (payload %u/%u bytes)", header->type, length - sizeof(ethernet_header_t), length);
-
-    logf("ethernet - dst %02X:%02X:%02X:%02X:%02X:%02X, src %02X:%02X:%02X:%02X:%02X:%02X",
-        header->dst[0], header->dst[1], header->dst[2], header->dst[3], header->dst[4], header->dst[5],
-        header->src[0], header->src[1], header->src[2], header->src[3], header->src[4], header->src[5]
-    );
+    length -= sizeof(ethernet_header_t);
 
     switch(header->type) {
-        case TYPE_IP: {
+        case ETHERNET_TYPE_IP: {
             ip_header_t *ip = (ip_header_t *) packet;
             packet += sizeof(ip_header_t);
-            logf("ethernet - ip packet detected (IPv%u)", IP_VERSION(ip->version_ihl));
-            logf("ethernet - ihl:%u dscp:%X ecn:%X total_len:%X ident:%X flags:%X frag_off:%X ttl:%X protocol:%X checksum:%X src:%X dst:%X", IP_IHL(ip->version_ihl) * 4, IP_DSCP(ip->dscp_ecn), IP_ECN(ip->dscp_ecn), ip->total_length, ip->ident, IP_FLAGS(ip->flags_frag_off), IP_FRAG_OFF(ip->flags_frag_off), ip->ttl, ip->protocol, ip->checksum, ip->src, ip->dst);
+            length -= sizeof(ip_header_t);
+
+            if(IP_VERSION(ip->version_ihl) != 0x04) {
+                logf("ethernet - ip packet with unsupported version number (0x%02X)", IP_VERSION(ip->version_ihl));
+            } else {
+                switch(ip->protocol) {
+                    case IP_TYPE_TCP: {
+                        tcp_header_t *tcp = (tcp_header_t *) packet;
+                        packet += sizeof(tcp_header_t);
+                        length -= sizeof(tcp_header_t);
+
+                        logf("ethernet - tcp packet detected");
+                        logf("ethernet - src %u.%u.%u.%u port %u dst %u.%u.%u.%u port %u",
+                            ip->src[0], ip->src[1], ip->src[2], ip->src[3], tcp->src_port,
+                            ip->dst[0], ip->dst[1], ip->dst[2], ip->dst[3], tcp->dst_port
+                        );
+
+                        logf("payload: %u", length);
+
+                        break;
+                    }
+                    case IP_TYPE_UDP: {
+                        //logf("ethernet - udp packet detected");
+                        break;
+                    }
+                    default: {
+                        logf("ethernet - ip packet with urecognised protocol (0x%02X)", ip->protocol);
+                        break;
+                    }
+                }
+            }
+
             break;
         }
-        case TYPE_ARP: {
+        case ETHERNET_TYPE_ARP: {
             logf("ethernet - arp packet detected");
             break;
         }
         default: {
-            logf("ethernet - unrecognised packet type (type:%02X)", header->type);
+            logf("ethernet - unrecognised packet type (0x%02X)", header->type);
             break;
         }
     }
