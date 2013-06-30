7c15265e9f412d251d45daac23dba7c355429f25
Keeley Hoek <escortkeel@gmail.com>
2013-06-30 22:16:11 +1000

    Added sys_log

--- src/task/syscall.c
+++ src/task/syscall.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 
+#include "string.h"
 #include "init.h"
 #include "syscall.h"
 #include "gdt.h"
@@ -8,6 +9,7 @@
 #include "asm.h"
 #include "task.h"
 #include "timer.h"
+#include "cache.h"
 #include "log.h"
 
 #define MAX_SYSCALL 256
@@ -34,10 +36,25 @@ static void sys_sleep(interrupt_t *interrupt) {
     task_switch();
 }
 
+static void sys_log(interrupt_t *interrupt) {
+    if(interrupt->registers.ecx > 1023) {
+        logf("syscall - task log string too long!");
+    } else {
+        char *buff = kmalloc(interrupt->registers.ecx + 1);    
+        memcpy(buff, (void *) interrupt->registers.ebx, interrupt->registers.ecx);
+        buff[interrupt->registers.ecx] = '\0';
+        
+        log(buff);
+        
+        kfree(buff, interrupt->registers.ecx + 1);
+    }
+}
+
 static syscall_t syscalls[MAX_SYSCALL] = {
     [0] = sys_exit,
     [1] = sys_fork,
-    [2] = sys_sleep
+    [2] = sys_sleep,
+    [3] = sys_log
 };
 
 static void syscall_handler(interrupt_t *interrupt) {
