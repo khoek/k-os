126182459ba675777f3bc723b7ee979747ef7c20
Keeley Hoek <escortkeel@gmail.com>
2013-07-07 13:14:28 +1000

    Added new rand functions

--- kernel/inc/rand.h
+++ kernel/inc/rand.h
@@ -4,6 +4,8 @@
 #include "int.h"
 
 void srand(uint32_t seed);
-uint8_t rand();
+uint8_t rand8();
+uint16_t rand16();
+uint32_t rand32();
 
 #endif
--- kernel/src/lib/rand.c
+++ kernel/src/lib/rand.c
@@ -9,8 +9,18 @@ void srand(uint32_t seed) {
     holdrand = seed;
 }
 
-uint8_t rand() {
+uint8_t rand8() {
     return (uint8_t) (((holdrand = holdrand * 214013L + 2531011L) >> 16) & 0x7fff);
+}
+
+uint16_t rand16() {
+    return (((uint16_t) rand8()) << 8)
+        | ((uint16_t) rand8());
+}
+
+uint32_t rand32() {
+    return (((uint32_t) rand16()) << 16)
+        | ((uint32_t) rand16());
 }
 
 static INITCALL rand_init() {
