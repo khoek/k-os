92d65d2f6c0058f6187b7061d5a5785168e69b53
Keeley Hoek <escortkeel@gmail.com>
2013-07-09 22:00:31 +1000

    Added ICMP ping support

--- kernel/src/net/icmp.c
+++ kernel/src/net/icmp.c
@@ -1,9 +1,11 @@
 #include "lib/int.h"
+#include "lib/rand.h"
 #include "lib/string.h"
 #include "mm/cache.h"
 #include "net/types.h"
 #include "net/layer.h"
 #include "net/protocols.h"
+#include "net/interface.h"
 #include "video/log.h"
 
 #include "checksum.h"
@@ -14,12 +16,12 @@ void layer_tran_icmp(packet_t *packet, uint8_t type, uint8_t code, uint32_t othe
     hdr->type = type;
     hdr->code = code;
     hdr->other = other;
-    
+
     uint32_t sum = 0;
     sum += hdr->type;
     sum += hdr->code;
     sum += hdr->other;
-    
+
     uint32_t len = packet->payload_size / sizeof(uint8_t);
     uint16_t *ptr = (uint16_t *) packet->payload;
     for (; len > 1; len -= 2) {
@@ -29,40 +31,40 @@ void layer_tran_icmp(packet_t *packet, uint8_t type, uint8_t code, uint32_t othe
     if(len) {
         sum += *((uint8_t *) ptr);
     }
-    
+
     hdr->checksum = sum_to_checksum(sum);
 
     packet->tran.icmp = hdr;
     packet->tran_size = sizeof(icmp_header_t);
 
-    layer_net_ip(packet, IP_PROT_ICMP, src_mac, dst_mac, src_ip, dst_ip);    
+    layer_net_ip(packet, IP_PROT_ICMP, src_mac, dst_mac, src_ip, dst_ip);
 }
 
 void recv_tran_icmp(net_interface_t *interface, packet_t *packet, void *raw, uint16_t len) {
     icmp_header_t *icmp = packet->tran.icmp = raw;
     raw = icmp + 1;
     len -= sizeof(icmp_header_t);
-    
+
     logf("icmp - type: %u code: %u", icmp->type, icmp->code);
-    
-    /*switch(icmp->type) {
+
+    switch(icmp->type) {
         case ICMP_TYPE_ECHO_REQUEST: {
-            switch(icmp->type) {
-                case ICMP_CODE_ECHO_REPLY: {
+            switch(icmp->code) {
+                case ICMP_CODE_ECHO_REQUEST: {
                     void *buff = kmalloc(len);
-                    memcpy(buff, packet, len);
-                    
-                    packet_t *reply = packet_alloc(buff, len);            
-                    layer_tran_icmp(reply, ICMP_TYPE_ECHO_REPLY, ICMP_CODE_ECHO_REPLY, interface->mac, ->sender_mac, interface->ip, arp->sender_ip);
+                    memcpy(buff, raw, len);
+
+                    packet_t *reply = packet_alloc(buff, len);
+                    layer_tran_icmp(reply, ICMP_TYPE_ECHO_REPLY, ICMP_CODE_ECHO_REPLY, icmp->other, interface->mac, packet->link.eth->src, interface->ip, packet->net.ip->src);
                     packet_send(interface, reply);
                     packet_free(reply);
-                    
+
                     kfree(buff, len);
-                    
+
                     break;
                 }
             }
             break;
         }
-    }*/
+    }
 }
