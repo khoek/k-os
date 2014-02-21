94700a32f037b8085900ae4cf93501361b4df128
Keeley Hoek <escortkeel@gmail.com>
2014-02-22 09:16:40 +1100

    Add init process stubs

--- kernel/src/init/kmain.c
+++ kernel/src/init/kmain.c
@@ -16,6 +16,19 @@
 
 multiboot_info_t *multiboot_info;
 
+static void user_init() {
+    kprintf("init - welcome to userland");
+
+    //TODO try to execve
+    // /sbin/init
+    // /etc/init
+    // /bin/init
+    // /bin/sh
+    // in that order
+
+    while(1);
+}
+
 void kmain(uint32_t magic, multiboot_info_t *mbd) {
     multiboot_info = mbd;
 
@@ -41,6 +54,10 @@ void kmain(uint32_t magic, multiboot_info_t *mbd) {
     kprintf("init - finished initcalls");
 
     mm_postinit_reclaim();
+
+    task_t *init = task_create("init", true, user_init, NULL);
+    //TODO remap the STD(IN/OUT/ERR) of this process to the screen
+    task_add(init);
 
     sched_run();
 }
