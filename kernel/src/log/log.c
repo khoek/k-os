e16b7859f919c49640d0a920565be26b045795a2
Keeley Hoek <escortkeel@gmail.com>
2016-02-01 12:27:02 +1100

    Rename log.c constants and move newline to front of line

--- kernel/src/log/log.c
+++ kernel/src/log/log.c
@@ -7,22 +7,22 @@
 #include "time/clock.h"
 #include "driver/console/console.h"
 
-#define LOGBUFFSIZE 32768
-#define TIMEBUFFSIZE 64
-#define LINEBUFFSIZE 1024
+#define LOG_BUFF_SIZE 32768
+#define TIME_BUFF_SIZE 64
+#define LINE_BUFF_SIZE 1024
 
 static uint32_t front;
-static char log_buff[LOGBUFFSIZE];
+static char log_buff[LOG_BUFF_SIZE];
 static DEFINE_SPINLOCK(log_lock);
 
-static char timestamp_buff[TIMEBUFFSIZE];
-static char line_buff[LINEBUFFSIZE];
+static char timestamp_buff[TIME_BUFF_SIZE];
+static char line_buff[LINE_BUFF_SIZE];
 static DEFINE_SPINLOCK(line_buff_lock);
 
 static void logbuff_append(const char *str, uint32_t len) {
     uint32_t coppied = 0;
     while(coppied < len) {
-        uint32_t chunk_len = MIN(len - coppied, LOGBUFFSIZE - front);
+        uint32_t chunk_len = MIN(len - coppied, LOG_BUFF_SIZE - front);
 
         memcpy(log_buff + front, str, chunk_len);
 
@@ -31,7 +31,7 @@ static void logbuff_append(const char *str, uint32_t len) {
         }
 
         coppied += chunk_len;
-        front = (front + chunk_len) % LOGBUFFSIZE;
+        front = (front + chunk_len) % LOG_BUFF_SIZE;
     }
 }
 
@@ -43,9 +43,9 @@ static void log(const char *str, uint32_t len) {
 
     uint32_t timestamp_len = sprintf(timestamp_buff, "[%5u.%03u] ", time / MILLIS_PER_SEC, time % MILLIS_PER_SEC);
 
+    logbuff_append("\n", 1);
     logbuff_append(timestamp_buff, timestamp_len);
     logbuff_append(str, len);
-    logbuff_append("\n", 1);
 
     spin_unlock_irqstore(&log_lock, flags);
 }
