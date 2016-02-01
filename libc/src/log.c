5b4d3fa36e1765c1be301955c490bf5d4c15db87
Keeley Hoek <escortkeel@gmail.com>
2016-02-01 12:28:36 +1100

    Rename libc/log.c constants

--- libc/src/log.c
+++ libc/src/log.c
@@ -3,8 +3,8 @@
 #include <k/sys.h>
 #include <k/log.h>
 
-#define BUFFSIZE 1024
-static char buff[BUFFSIZE];
+#define KPRINTF_BUFF_SIZE 1024
+static char buff[KPRINTF_BUFF_SIZE];
 
 void kprint(const char *str) {
     _log(str, strlen(str));
