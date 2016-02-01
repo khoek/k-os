0628631feb3897f95da4cfaa344f9d49a75af84b
Keeley Hoek <escortkeel@gmail.com>
2016-02-01 12:28:57 +1100

    Remove broken keyboard code
    GRUB does this for us anyway (for its UI)

--- kernel/src/driver/console/keyboard.c
+++ kernel/src/driver/console/keyboard.c
@@ -85,21 +85,6 @@ read_retry:
 
 #define CTRL_STATUS_INBUF (1 << 1)
 
-static void ctrl_send_cmd(uint8_t cmd) {
-	while(inb(CTRL_REG) & CTRL_STATUS_INBUF);
-	outb(CTRL_REG, cmd);
-}
-
-static uint8_t ctrl_read_status() {
-	while(inb(CTRL_REG) & CTRL_STATUS_INBUF);
-	return inb(ENCR_REG);
-}
-
-static void encr_send_cmd(uint8_t cmd) {
-	while(inb(CTRL_REG) & CTRL_STATUS_INBUF);
-	outb(ENCR_REG, cmd);
-}
-
 uint8_t encr_read() {
 	while(inb(CTRL_REG) & CTRL_STATUS_INBUF);
 	return inb(ENCR_REG);
@@ -111,27 +96,6 @@ static void handle_keyboard(interrupt_t *interrupt, void *data) {
 
 void keyboard_init(console_t *console) {
     the_console = console;
-
-    ctrl_send_cmd(0xAE);
-    encr_read();
-    ctrl_send_cmd(0x20);
-    encr_read();
-
-    encr_send_cmd(0xFF); //Reset and start self-test
-    encr_read(); //0xFA = ACK
-    encr_read(); //0xAA = SUCCESS
-
-    encr_send_cmd(0xEE); //ECHO
-    encr_read(); //0xEE = ECHO
-
-    ctrl_send_cmd(0xAA); //Test PS/2 Controller
-    if(ctrl_read_status() != 0x55) { //0x55 = SUCCESS
-        kprintf("kbd - controller failed BIST!");
-    }
-
-    encr_send_cmd(0xF4);
-    encr_read();
-    encr_read();
 
     register_isr(KEYBOARD_IRQ + IRQ_OFFSET, CPL_KRNL, handle_keyboard, NULL);
 }
