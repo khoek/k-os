a22c078b2ee78cad0b52a661e37c2ec98720161d
Keeley Hoek <escortkeel@gmail.com>
2016-02-01 12:30:02 +1100

    Add barebones shell libc/init program

--- libc/init.c
+++ libc/init.c
@@ -6,19 +6,94 @@
 #include <string.h>
 #include <unistd.h>
 
-void dowrite(int fd, char *str) {
-    write(fd, str, strlen(str));
+void puts(char *str) {
+    write(1, str, strlen(str));
 }
 
-int main() {
-    for(int i = 0; i < 25; i++) {
-        dowrite(0, "\n");
-    }
+void putc(char c) {
+    write(1, &c, 1);
+}
 
+char getc() {
+    char c;
+    read(0, &c, 1);
+    return c;
+}
+
+#define CSI_DOUBLE_PARTA ((char) 0x1B)
+#define CSI_DOUBLE_PARTB ((char) '[')
+
+static void send_backspace_esc() {
+    putc(CSI_DOUBLE_PARTA);
+    putc(CSI_DOUBLE_PARTB);
+    putc('D');
+
+    putc(' ');
+
+    putc(CSI_DOUBLE_PARTA);
+    putc(CSI_DOUBLE_PARTB);
+    putc('D');
+}
+
+void print_welcome() {
+    putc(CSI_DOUBLE_PARTA);
+    putc(CSI_DOUBLE_PARTB);
+    putc('2');
+    putc('J');
+
+    putc(CSI_DOUBLE_PARTA);
+    putc(CSI_DOUBLE_PARTB);
+    putc('1');
+    putc(';');
+    putc('1');
+    putc('H');
+
+    puts("Welcome to KSH!\n");
+    puts("Copyright (c) 2016, Keeley Hoek.\n");
+    puts("All rights reserved.\n\n");
+}
+
+void print_linestart() {
+    puts("\nroot@k-os:/$ ");
+}
+
+void execute_command(char *cmd) {
+    kprintf("executing: %s", cmd);
+}
+
+int main(int argc, char **argv) {
+    print_welcome();
+    print_linestart();
+
+    uint32_t off = 0;
+    char buff[512];
     while(1) {
-        char c;
-        read(0, &c, 1);
-        write(0, &c, 1);
+        char c = getc();
+
+        switch(c) {
+            case '\x7f': {
+                if(off) {
+                    off--;
+                    send_backspace_esc();
+                }
+
+                break;
+            }
+            case '\n': {
+                buff[off] = '\0';
+                if(off) execute_command(buff);
+                off = 0;
+                print_linestart();
+
+                break;
+            }
+            default: {
+                putc(c);
+                buff[off++] = c;
+
+                break;
+            }
+        }
     }
 
     return 1;
